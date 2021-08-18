eef6a7489067f0ae23bb5e83af8f4fb46cf14188
grantbuster <grant.buster@nrel.gov>
2021-08-18 09:56:52 -0600

    added script to collect images into a final data dir

---
+++ collect_final_data.py
@@ -0,0 +1,43 @@
+import numpy as np
+import os
+import shutil
+from glob import glob
+
+data_dir = './data/data/'
+numerals = ["i", "ii", "iii", "iv", "v", "vi", "vii", "viii", "ix", "x"]
+
+def clean_num_dir(num_dir):
+    for f in os.listdir(num_dir):
+        fp = os.path.join(num_dir, f)
+        if os.path.isdir(fp):
+            shutil.rmtree(fp)
+
+
+for n in numerals:
+    val_dir = data_dir + '/val/{}/'.format(n)
+    train_dir = data_dir + '/train/{}/'.format(n)
+    vae_dir = data_dir + '/vae_out/{}/'.format(n)
+
+    clean_num_dir(val_dir)
+    clean_num_dir(train_dir)
+    clean_num_dir(vae_dir)
+
+    train_files = glob(train_dir + '*.png')
+    val_files = glob(val_dir + '*.png')
+    vae_files = glob(vae_dir + '*.png')
+
+    n_tot_files = len(train_files) + len(val_files)
+    n_vae_files = 1000 - n_tot_files
+
+    choices = np.random.choice(np.arange(n_vae_files), n_vae_files,
+                               replace=False)
+
+    for i in choices:
+        fp_source = vae_files[i]
+        fp_dest = fp_source.replace('vae_out', 'train')
+        shutil.copy(fp_source, fp_dest)
+
+    train_files = glob(train_dir + '*.png')
+    val_files = glob(val_dir + '*.png')
+    n_tot_files = len(train_files) + len(val_files)
+    assert n_tot_files == 1000
