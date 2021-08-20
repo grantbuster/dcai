3eda8958a2961ba80c9f6c55b21beae2bfaf0155
grantbuster <grant.buster@nrel.gov>
2021-08-20 17:01:04 -0600

    outputs for dcai_gcb_05, best so far

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 CURRENT_DATE=`date +"%Y%m%d%H"`
-python train.py dcai_gcb_04 label_book > ./logs/train_log_${CURRENT_DATE}.log data label_book
+python train.py dcai_gcb_05 label_book > ./logs/train_log_${CURRENT_DATE}.log data label_book
